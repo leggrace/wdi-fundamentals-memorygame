685bae48f286f2c7b24d7131ecc5be8436b8dbd5
Germond <Ehryn.Germond@disney.com>
2018-02-09 12:38:59 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,19 +25,30 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert ("You have found a match!");
+    alert ("You have found a match! Do you want to play again?");
   } else {
     alert ("Sorry, try again.");
+    flipcard.reload();
   }
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
 console.log("User flipped" + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
   checkForMatch();
 }
+}
+var createBoard = function (){
+for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard)
+document.getElementById('game-board').appendChild(cardElement);
+}
 };
-flipCard(0)
-flipCard(2)
+createBoard();
